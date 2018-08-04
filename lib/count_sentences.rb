e3b6632ec73e28ac7176d12b605be285affcdaf4
Karen C <chenyanhuan66@163.com>
2018-08-04 03:28:00 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1]=="!"||self[-1]=="!"||self[-1]=="."
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self[-1] == "?"
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    num = 0
+    index = 0
+    while self[index] != nil
+      if self[index]!= slef[index+1] && 
+        (self[index]=="!"||self[index]=="."||self[index]=="?")
+        num += 1
+      end
+    end
+    return num
   end
+  
 end
